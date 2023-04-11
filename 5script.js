996b46c530a4c393c564a70af88400aca09a8798
elexio2 <119396078+elexio2@users.noreply.github.com>
2023-04-11 10:06:25 +0300

    Add files via upload

--- 5script.js
+++ 5script.js
@@ -1,5 +1,29 @@
 const plusins = document.querySelector('.js-plus');
-const inputins = document.querySelector('.js-input');
+const multiply = document.querySelector('.js-multiply');
+const minus = document.querySelector('.js-minus');
+const divide = document.querySelector('.js-divide');
+const inputins1 = document.querySelector('.js-input1');
+const inputins2 = document.querySelector('.js-input2');
+const answer = document.querySelector('.js-result');
 
-console.log(plusins)
-console.log(inputins)
+const Add = (cipars1, cipars2) => {
+    return cipars1 + cipars2
+}
+
+const Subtract = (cipars1, cipars2) => {
+    return cipars1 - cipars2
+}
+
+const Multiply = (cipars1, cipars2) => {
+    return cipars1 * cipars2
+}
+
+const Divide = (cipars1, cipars2) => {
+    return cipars1 / cipars2
+}
+
+plusins.addEventListener('click', () => {
+    console.log(Add
+        (parseInt(inputins1.value)
+        ,parseInt(inputins2.value)))
+})
