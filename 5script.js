a12d7f64307d4857ecdbdd6bc85a94409e2b8cc0
elexio2 <119396078+elexio2@users.noreply.github.com>
2023-04-18 08:40:12 +0300

    Add files via upload

--- 5script.js
+++ 5script.js
@@ -21,9 +21,39 @@ const Multiply = (cipars1, cipars2) => {
 const Divide = (cipars1, cipars2) => {
     return cipars1 / cipars2
 }
+const clear = () => {
+    inputins1.value = ""
+    inputins2.value = ""
+}
 
 plusins.addEventListener('click', () => {
-    console.log(Add
-        (parseInt(inputins1.value)
-        ,parseInt(inputins2.value)))
+    answer.textContent = Add
+    (parseInt(inputins1.value),
+    parseInt(inputins2.value))
+
+    clear()
+})
+
+minus.addEventListener('click', () =>{
+    answer.textContent = Subtract
+    (parseInt(inputins1.value),
+    parseInt(inputins2.value))
+
+    clear
+})
+
+multiply.addEventListener ('click', () =>{
+    answer.textContent = Multiply
+    (parseInt(inputins1.value),
+    parseInt(inputins2.value))
+
+    clear
+})
+
+divide.addEventListener ('click', () =>{
+    answer.textContent = Divide
+    (parseInt(inputins1.value),
+    parseInt(inputins2.value))
+
+    clear
 })
